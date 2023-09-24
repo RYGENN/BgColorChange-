37019e140b6638b3fa7a753afdfdc171c89caf9f
anupam <anupammahato357@gmail.com>
2023-09-24 19:25:04 +0530

    third commit

---
+++ demo.js
@@ -0,0 +1,21 @@
+const mainButton=document.querySelector("button");
+console.log(mainButton)
+const body=document.body
+const tag=document.querySelector('.current-color')
+
+
+function randomColorGenerator () {
+    const red=Math.floor(Math.random()*256);
+    const green=Math.floor(Math.random()*256);
+    const blue=Math.floor(Math.random()*256);
+    const randomColor=`rgb(${red},${green},${blue})`
+    return randomColor
+    
+}
+
+mainButton.addEventListener('click',() =>{
+    randomColor=randomColorGenerator();
+    body.style.backgroundColor=randomColor;
+    tag.textContent=randomColor;  
+   
+})  
