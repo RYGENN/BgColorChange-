1345ebd25871f962572b02be31b8347bc9446c6e
anupam <anupammahato357@gmail.com>
2023-09-25 22:51:05 +0530

    some change

--- demo.js
+++ demo.js
@@ -5,9 +5,9 @@ const tag=document.querySelector('.current-color')
 
 
 function randomColorGenerator () {
-    const red=Math.floor(Math.random()*256);
-    const green=Math.floor(Math.random()*256);
-    const blue=Math.floor(Math.random()*256);
+    const red=Math.floor(Math.random()*255);
+    const green=Math.floor(Math.random()*255);
+    const blue=Math.floor(Math.random()*255);
     const randomColor=`rgb(${red},${green},${blue})`
     return randomColor
     
